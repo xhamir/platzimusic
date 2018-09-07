c45563a17dbf46d96643686352af13edd71aad42
Omar Montalvo <xhamir.one@gmail.com>
2018-09-07 13:45:01 -0500

    Fix url github pages

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   #app
-    img(src='./assets/logo.png')
+    img(src='https://xhamir.github.io/platzimusic/dist/logo.png')
     h1 PlatziMusic
     select(v-model="selectedCountry")
       option(v-for="country in countries" :value="country.value") {{ country.name}}
